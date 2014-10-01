2a7f7f97538ac4a3123a6c07e953f68e7f77e070
bemoliph <Bemoliph@users.noreply.github.com>
2014-10-01 22:51:47 +0000

    README.md edited online with Bitbucket

---
+++ README.md
@@ -0,0 +1,17 @@
+# README #
+
+This is an alternative client downloader for Elite: Dangerous.  Although the official launcher is still required to log in and play, this script allows players to bypass the launcher's tendency to be unresponsive or stall out for very long periods while downloading.
+
+### Requirements ###
+
+* Python 2.x (made with 2.7, but should work with earlier versions)
+
+### Usage ###
+
+1. Set manifestURL to the URL of the latest patch manifest.
+2. Set downloadDir to the installation directory for Elite: Dangerous (e.g., "E:\\Program Files (x86)\\Frontier\\EDLaunch\\Products\\FORC-FDEV-D-1002").  Note the use of \\ versus \ because Python and Windows.
+3. Run the script and enjoy your launcherless download!
+
+### Troubleshooting ###
+* Is there any way to verify the files were downloaded correctly?
+Log into the official launcher, then click your e-mail address > Validate Game Files.
---
+++ downloader.py
@@ -0,0 +1,61 @@
+import gzip
+import hashlib
+import StringIO
+import os
+import urllib
+
+from xml.etree import ElementTree
+
+def generateDirectories(path):
+	dir = os.path.dirname(path)
+	
+	if not os.path.isdir(dir):
+		os.makedirs(dir)
+
+def getPatchManifest(manifestURL):
+	compressedManifest = StringIO.StringIO(urllib.urlopen(manifestURL).read())
+	decompressedManifest = gzip.GzipFile(fileobj=compressedManifest)
+	parsedManifest = ElementTree.fromstring(decompressedManifest.read())
+	
+	return parsedManifest
+
+def alreadyHaveAsset(downloadPath, assetHash):
+	if os.path.isfile(downloadPath):
+		# Asset exists on disk, but need to check version
+		sha1 = hashlib.sha1()
+		sha1.update(open(downloadPath, "rb").read())
+		localHash = sha1.hexdigest()
+		
+		return localHash == assetHash
+	else:
+		# Asset doesn't exist on disk
+		return False
+
+def downloadAssets(manifest, downloadDir):
+	assetCount = len(manifest)
+
+	# Get assets
+	for assetNumber, asset in enumerate(manifest.getchildren()):
+		assetURL = asset.find("Download").text
+		assetSize = float(asset.find("Size").text) / 1048576.0 # Bytes -> Megabytes
+		assetHash = asset.find("Hash").text
+		assetRelPath = asset.find("Path").text
+		downloadPath = os.path.join(downloadDir, assetRelPath)
+		
+		generateDirectories(downloadPath)
+		
+		if alreadyHaveAsset(downloadPath, assetHash):
+			print "Skipping file %d of %d: (%.2f MB) %s" % (assetNumber+1, assetCount, assetSize, assetRelPath)
+		else:
+			print "Downloading file %d of %d: (%.2f MB) %s" % (assetNumber+1, assetCount, assetSize, assetRelPath)
+			urllib.urlretrieve(assetURL, downloadPath)
+
+if __name__ == "__main__":
+	# TODO: Automatically determine manifest URL
+	manifestURL = "http://cdn.zaonce.net/elitedangerous/win/manifests/Beta2.02_Final+%282014.10.01.44046%29.xml.gz"
+	# TODO: Automatically determine download directory
+	downloadDir = "E:\\Program Files (x86)\\Frontier\\EDLaunch\\Products\\FORC-FDEV-D-1002"
+	
+	generateDirectories(downloadDir)
+	manifest = getPatchManifest(manifestURL)
+	downloadAssets(manifest, downloadDir)
